7503abad724ae67597f3d657a2d302d9f045ffd2
Chen <chen.jiang.1206@gmail.com>
2023-06-05 16:35:22 -0400

    models draft done

--- models.py
+++ models.py
@@ -1,5 +1,172 @@
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
 
-from config import db
+from config import db, bcrypt
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+
+    #Serialize_rules
+    serialize_rules = ("-cakes.users", "-reviews.user", "-favorite_cakes.user", "-orders.user",)
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+
+
+    #Relationships:
+    reviews = db.relationship('Review', back_populates="user", cascade="all, delete-orphan")
+    favorite_cakes = db.relationship('FavoriteCake', back_populates="user", cascade="all, delete-orphan")
+    orders = db.relationship('Order', back_populates="user", cascade="all, delete-orphan")
+
+    #Association_proxy:
+    cakes = association_proxy('reviews', 'cake', creator=lambda cake_obj: Review(cake=cake_obj))
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("Password hashes can't be viewed")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8') )
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8')
+        )
+
+    def __repr__(self):
+        return f'<User: {self.id} {self.username}'
+
+class Cake(db.Model, SerializerMixin):
+    __tablename__ = "cakes"
+
+    serialize_rules = ("-users.cakes", "-reviews.cake", "-favorite_cakes.cake", "-order_cakes.cake", "-orders.cakes", )
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    price = db.Column(db.Numeric(8, 2), nullable=False)
+    description = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable=False)
+
+    #Relationships:
+    reviews = db.relationship('Review', back_populates="cake", cascade="all, delete-orphan")
+    favorite_cakes = db.relationship('FavoriteCake', back_populates="cake", cascade="all, delete-orphan")
+    order_cakes = db.relationship('OrderCake', back_populates="cake", cascade="all, delete-orphan")
+
+    #Association_proxy
+    users = association_proxy('reviews', 'user', creator=lambda user_obj: Review(user=user_obj))
+    orders = association_proxy('order_cakes', 'order', creator=lambda order_obj: OrderCake(order=order_obj))
+
+
+    @validates('description')
+    def check_length(self, key, description):
+        if description >= 250:
+            return description
+        raise ValueError('Description needs to be longer than 250 chars')
+
+    def __repr__(self):
+        return f'<Cake: {self.id} {self.name}>'
+
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = "reviews"
+
+    serialize_rules = ("-cake.reviews", "-user.reviews",)
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    cake_id = db.Column(db.Integer, db.ForeignKey('cakes.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    #Relationships:
+    user = db.relationship('User', back_populates="reviews")
+    cake = db.relationship('Cake', back_populates="reviews")
+
+
+    def __repr__(self):
+        return f'Review: {self.id}'
+
+class FavoriteCake(db.Model, SerializerMixin):
+    __tablename__ = "favorite_cakes"
+
+    serialize_rules = ("-cake.favorite_cakes", "-user.favorite_cakes",)
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    cake_id = db.Column(db.Integer, db.ForeignKey('cakes.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    #Relationships:
+    user = db.relationship('User', back_populates="favorite_cakes")
+    cake = db.relationship('Cake', back_populates="favorite_cakes")
+
+    def __repr__(self):
+        return f'<FavoriteCake: {self.id} {self.cake.name}>'
+
+class Order(db.Model, SerializerMixin):
+    __tablename__ = "orders"
+
+    serialize_rules = ("-user.orders", "-order_cakes.order", "-cakes.orders")
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    #figure out how to calculate this automatically
+    _total_price = db.Column(db.Numeric(8, 2), default=0)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    #Relationships:
+    user = db.relationship('User', back_populates="orders")
+    order_cakes = db.relationship('OrderCake', back_populates="order", cascade="all, delete-orphan")
+
+    #Associatio_proxy:
+    cakes = association_proxy('order_cakes', 'cake', creator=lambda cake_obj: OrderCake(cake=cake_obj))
+
+    @property
+    def total_price(self):
+        return self._total_price
+
+    #need to double check here
+    @total_price.setter
+    def total_price(self):
+        self._total_price = sum([oc.price for oc in self.order_cakes])
+
+    def __repr__(self):
+        return f'<Order: {self.id}>'
+
+class OrderCake(db.Model, SerializerMixin):
+    __tablename__ = "order_cakes"
+
+    serialize_rules = ("-order.order_cakes", "-cake.order_cakes",)
+
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer, default=0)
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'))
+    cake_id = db.Column(db.Integer, db.ForeignKey('cakes.id'))
+    _price = db.Column(db.Numeric(8, 2), default=0)
+
+    #Relationships:
+    order = db.relationship('Order', back_populates="order_cakes")
+    cake = db.relationship('Cake', back_populates="order_cakes")
+
+
+    @property
+    def price(self):
+        return self._price
+
+    #need to double check here
+    @price.setter
+    def price(self):
+        self._price = self.quantity * self.cake.price
+
+    def __repr__(self):
+        return f'<OrderCake: {self.id}>'
--- seed.py
+++ seed.py
@@ -8,10 +8,80 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User, Cake, Review, FavoriteCake, Order, OrderCake
+
+fake = Faker()
+
+def create_users():
+    users = []
+    usernames = []
+    emails = []
+    for _ in range(10):
+        username = fake.first_name()
+        while username in usernames:
+            username = fake.first_name()
+        usernames.append(username)
+
+        email = fake.email()
+        while email in emails:
+            email = fake.email()
+        emails.append(email)
+
+        user = User(
+            username=username,
+            email=email
+        )
+
+        user.password_hash = user.username + "hello"
+        users.append(user)
+    db.session.add_all(users)
+    db.session.commit()
+
+def create_cakes():
+    cakes = []
+    cake_names = [
+    'Chocolate Cake',
+    'Vanilla Cake',
+    'Strawberry Cake',
+    'Red Velvet Cake',
+    'Rainbow Cake',
+    'Great British Baking Show Cake',
+    'Princess Cake',
+    'Holiday Cake',
+    'Cup Cake']
+
+    for i in range(len(cake_names)):
+        cake = Cake(
+            name=cake_names[i],
+            price=randint(20, 50),
+            description=fake.paragraph(nb_sentence=10),
+            image=fake.image_url()
+        )
+        cakes.append(cake)
+
+    db.session.add_all(cakes)
+    db.session.commit()
+
+def create_reviews():
+    pass
+
+def create_favorites():
+    pass
+
+def create_orders():
+    pass
+
+def create_order_cakes():
+    pass
+
 
 if __name__ == '__main__':
-    fake = Faker()
+
     with app.app_context():
         print("Starting seed...")
-        # Seed code goes here!
+        User.query.delete()
+        Cake.query.delete()
+        Review.query.delete()
+        FavoriteCake.query.delete()
+        Order.query.delete()
+        OrderCake.query.delete()
