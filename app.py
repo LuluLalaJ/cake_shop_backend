2a06356a9316e4160b51d7fd5af6d051abc5beb3
Chen <chen.jiang.1206@gmail.com>
2023-06-06 11:37:01 -0400

    favorite endpoints done

--- app.py
+++ app.py
@@ -80,8 +80,62 @@ class CakesById(Resource):
             return cake.to_dict(only=("id", "name", "price", "description", "image", "reviews")), 200
         return {'error': 'Cake not found'}, 404
 
+class Favorites(Resource):
+    def get(self):
+        if session.get('user_id'):
+            favorites = FavoriteCake.query.filter_by(user_id=session['user_id']).all()
+            favorites_serialized = [fav.to_dict(rules=('-cake.reviews','-cake.order_cakes', '-user')) for fav in favorites]
+            return favorites_serialized, 200
+        return {'error': '401 Unauthorized'}, 401
+
+    def post(self):
+        if session.get('user_id'):
+            cake_id = request.get_json()['cake_id']
+            user_id = session['user_id']
+            already_faved = FavoriteCake.query.filter_by(cake_id=cake_id, user_id=user_id).first()
+            if already_faved:
+                return {'error': 'Already a favorite'}, 400
+            try:
+                new_fav = FavoriteCake(
+                    cake_id=cake_id,
+                    user_id=user_id
+                )
+                db.session.add(new_fav)
+                db.session.commit()
+                return new_fav.to_dict(rules=('-cake.reviews','-cake.order_cakes', '-user')), 201
+            except IntegrityError:
+                return {'error': 'invalid input'}, 422
+        return {'error': '401 Unauthorized'}, 401
+
+class FavoritesById(Resource):
+    def get(self, id):
+        if session.get('user_id'):
+            user_id = session['user_id']
+            favorite = FavoriteCake.query.filter_by(id=id, user_id=user_id).first()
+            if favorite:
+                return favorite.to_dict(rules=('-cake.reviews','-cake.order_cakes', '-user')), 200
+            return {'error': 'favorite cake is not found'}, 404
+        return {'error': '401 Unauthorized'}, 401
+
+
+    def delete(self, id):
+        if session.get('user_id'):
+            user_id = session['user_id']
+            favorite = FavoriteCake.query.filter_by(id=id, user_id=user_id).first()
+            try:
+                db.session.delete(favorite)
+                db.session.commit()
+                return {}, 204
+            except:
+                return {'error': 'favorite cake is not found'}, 404
+        return {'error': '401 Unauthorized'}, 401
+
 class Order(Resource):
     pass
+
+
+
+
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
@@ -89,6 +143,8 @@ api.add_resource(Login, '/login', endpoint='login')
 api.add_resource(Logout, '/logout', endpoint='logout')
 api.add_resource(Cakes, '/cakes', endpoint='cakes')
 api.add_resource(CakesById, '/cakes/<int:id>', endpoint='/cakes/<int:id>')
+api.add_resource(Favorites, '/favorites', endpoint='favorites')
+api.add_resource(FavoritesById, '/favorites/<int:id>', endpoint='/favorites/<int:id>')
 
 
 
