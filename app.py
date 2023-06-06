1e3fc96aa3a53cc82c06506b856ee4257dab64a8
Chen <chen.jiang.1206@gmail.com>
2023-06-05 20:10:10 -0400

    set up app.py

--- app.py
+++ app.py
@@ -3,14 +3,30 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response, session
 from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
 
 # Local imports
 from config import app, db, api
-from models import User
+from models import db, User, Cake, Review, FavoriteCake, Order, OrderCake
 
-# Views go here!
+class Signup(Resource):
+    pass
+
+class CheckSession(Resource):
+    pass
+
+class Login(Resource):
+    pass
+
+class Logout(Resource):
+    pass
+
+api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
